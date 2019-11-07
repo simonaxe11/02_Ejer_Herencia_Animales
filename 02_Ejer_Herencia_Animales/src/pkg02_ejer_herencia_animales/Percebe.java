c5ffebd6735863c62fbcd9e95d3c02380dcbe6e4
jorge <jorge.garcia.lalin@gmail.com>
2019-11-07 17:13:13 +0100

    clase percebe

---
+++ 02_Ejer_Herencia_Animales/src/pkg02_ejer_herencia_animales/Percebe.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg02_ejer_herencia_animales;
+
+/**
+ *
+ * @author PC
+ */
+public class Percebe {
+    
+    String alimento = "Fitoplacton";
+    
+    
+    public void alimentar(){
+        System.out.println(" se nutre de " + alimento);
+    }
+    
+}
